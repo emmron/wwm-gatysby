192c522d2d11c69d4ecb7533e0c624af769f0ef7
Emmett Hoolahan <emmetthoolahan@gmail.com>
2021-02-25 15:55:44 +0800

    added logo to hero changed hero made it better lol

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import {Col, Container, Row} from 'react-bootstrap';
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-scroll';
 import PortfolioContext from '../../context/context';
+import Image from "../Image/Image";
 
 const Hero = () => {
   const { hero } = useContext(PortfolioContext);
@@ -24,9 +25,22 @@ const Hero = () => {
   return (
     <section id="hero" className="jumbotron">
       <Container>
+
+        <Row className="hero-img">
+          <Col className="hero-imgcol" md={12} sm={12}>
+            <Image
+                src="logoamber.svg"
+                className="mx-auto shadow-xl"
+                alt="Sunset Image"
+                style={{
+                  border: 'none',
+                }}
+            />
+          </Col>
+        </Row>
+
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            <span className="main-title">{title || 'WA Web Masters'}{' '}</span>
             <span className="text-color-main">{name || 'Building premium websites with an appreciation for the craft'}</span>
             <span className="subtitle">{subtitle || "Website Builds of the highest Caliber"}</span>
           </h1>
