4202cc523b56a3d39e4e1b0e000988398cd4ae7c
Emmett Hoolahan <emmetthoolahan@gmail.com>
2021-02-25 11:30:27 +0800

    stuff

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,6 +5,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
 import Title from '../Title/Title';
 import ProjectImg from '../Image/ProjectImg';
+import Image from "../Image/Image";
 
 const Projects = () => {
   const { projects } = useContext(PortfolioContext);
@@ -23,96 +24,31 @@ const Projects = () => {
   }, []);
 
   return (
-    <section id="projects">
-      <Container>
-        <div className="project-wrapper">
-          <Title title="Our Services" />
-          {projects.map((project) => {
-            const { title, info, info2, url, repo, img, id } = project;
+      <section id="projects">
+        <Container>
+          <Title title="Why our builds are the best in Perth" />
+          <Row className="about-wrapper">
+            <Col md={4} sm={12}>
+              <Fade bottom duration={1000} delay={600} distance="30px">
+                <div className="about-wrapper__image">
+                  <Image
+                      src="ferrari.png"
+                      className="mx-auto shadow-xl"
+                      alt="Sunset Image"
+                      style={{
+                        border: 'none',
+                        height: '300px',
+                      }}
+                  />
+                </div>
+              </Fade>
+            </Col>
+            <Col md={6} sm={12}>
 
-            return (
-              <Row key={id}>
-                <Col lg={4} sm={12}>
-                  <Fade
-                    left={isDesktop}
-                    bottom={isMobile}
-                    duration={1000}
-                    delay={500}
-                    distance="30px"
-                  >
-                    <div className="project-wrapper__text">
-                      <h3 className="project-wrapper__text-title">{title || 'Project Title'}</h3>
-                      <div>
-                        <p>
-                          {info ||
-                            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
-                        </p>
-                        <p className="mb-4">{info2 || ''}</p>
-                      </div>
-                      <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="cta-btn cta-btn--hero"
-                        href={url || '#!'}
-                      >
-                        See Live
-                      </a>
-
-                      {repo && (
-                        <a
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="cta-btn text-color-main"
-                          href={repo}
-                        >
-                          Source Code
-                        </a>
-                      )}
-                    </div>
-                  </Fade>
-                </Col>
-                <Col lg={8} sm={12}>
-                  <Fade
-                    right={isDesktop}
-                    bottom={isMobile}
-                    duration={1000}
-                    delay={1000}
-                    distance="30px"
-                  >
-                    <div className="project-wrapper__image">
-                      <a
-                        href={url || '#!'}
-                        target="_blank"
-                        aria-label="Project Link"
-                        rel="noopener noreferrer"
-                      >
-                        <Tilt
-                          options={{
-                            reverse: false,
-                            max: 8,
-                            perspective: 1000,
-                            scale: 1,
-                            speed: 300,
-                            transition: true,
-                            axis: null,
-                            reset: true,
-                            easing: 'cubic-bezier(.03,.98,.52,.99)',
-                          }}
-                        >
-                          <div data-tilt className="thumbnail rounded">
-                            <ProjectImg alt={title} filename={img} />
-                          </div>
-                        </Tilt>
-                      </a>
-                    </div>
-                  </Fade>
-                </Col>
-              </Row>
-            );
-          })}
-        </div>
-      </Container>
-    </section>
+            </Col>
+          </Row>
+        </Container>
+      </section>
   );
 };
 
